fceec0a813cfc6e6f077f1babbbd39585d4d2dba
azat-sengul <azat.sengul@hotmail.com>
2023-10-15 17:37:39 +0300

    Repository ile veri static veriler eklendi. Buna bağlı olarak listeleme ve filtreleme işlemleri view üzerinde yapılıdı

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FormsApp.Controllers;
+
+public class HomeController : Controller
+{
+  
+
+    public HomeController()
+    {
+        
+    }
+
+    public IActionResult Index(string searchString, string category) // searchString ile get metodu yardımı ile sayfa arama filtreleme yapılır
+    {
+        var products = Repository.Products;
+
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            ViewBag.SearchString = searchString;
+
+            products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+        }
+        // View üzerinde kategori listeleme için yapılacak
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name"); //Görünür name, value CategoryId olur
+
+        if(!String.IsNullOrEmpty(category) && category !="0")
+        {
+           
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+
+        return View(products);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsApp.Models
+{
+    public class Product
+    {
+        [Display (Name="Ürün ID")]
+        public int ProductId { get; set; }
+
+        [Display (Name="Ürün Adı")]
+        public string? Name { get; set; }
+
+        [Display (Name="Fiyat")]
+        public decimal Price { get; set; }
+
+        [Display (Name="Resim")]
+        public string? Image { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
